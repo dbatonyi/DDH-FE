f07db0b88a600bc1b9f1d4473d38b16a1b589a95
Donát Bátonyi <dbatonyi@gmail.com>
2022-05-26 18:33:35 +0200

    DDH FE - Create Task Form (BEV-0.2.4)

--- pages/task/new.js
+++ pages/task/new.js
@@ -1,36 +1,47 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
+import { TagsInput } from 'react-tag-input-component';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
-import Layout from '../../layouts/Layout';
+import Select from 'react-select';
 import configData from '../../config.json';
 import { AuthContext } from '../../layouts/Layout';
 
 const NewTask = (props) => {
     const router = useRouter();
     const authContext = useContext(AuthContext);
-
-    console.log(authContext);
+    const [tag, setTag] = useState([]);
 
     const submit = async (e) => {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget);
 
-        const email = formData.get('email');
-        const password = formData.get('password');
+        const title = formData.get('title');
+        const short = formData.get('short');
+        const body = formData.get('body');
+        const category = formData.get('category');
 
-        await fetch(`${configData.SERVER_URL}/api/login`, {
+        await fetch(`${configData.SERVER_URL}/api/task/new`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
             body: JSON.stringify({
-                email: email,
-                password: password
+                title: title,
+                taskCategory: category,
+                taskTags: tag.toString(),
+                taskShort: short,
+                taskDescription: body,
+                userUuid: authContext.userInfo.uuid
             })
         });
 
         await router.push('/');
     };
+
+    const categoryOptions = [
+        { value: 'frontend', label: 'Frontend' },
+        { value: 'backend', label: 'Backend' },
+        { value: 'test', label: 'Test' }
+    ];
 
     return (
         <form onSubmit={submit}>
@@ -39,8 +50,17 @@ const NewTask = (props) => {
             <label htmlFor='title'>Title</label>
             <input className='text' name='title' type='text' required />
 
-            <label htmlFor='body'>Text</label>
+            <label htmlFor='short'>Task Short</label>
+            <textarea className='text' name='short' type='textarea' required />
+
+            <label htmlFor='body'>Task Description</label>
             <textarea className='text' name='body' type='textarea' required />
+
+            <label htmlFor='category'>Task Category</label>
+            <Select name='category' options={categoryOptions} />
+
+            <label htmlFor='tag'>Task Tags</label>
+            <TagsInput value={tag} onChange={setTag} name='tag' placeHolder='enter tags' />
 
             <button type='submit'>Save</button>
         </form>
