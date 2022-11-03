7e50567bacbfcc0be309767709164ab8993f978d
dbatonyi <dbatonyi@gmail.com>
2022-11-03 17:05:48 +0100

    DDH FE - Tags list added with description (BEV-1.0.2)

--- pages/tags.js
+++ pages/tags.js
@@ -13,103 +13,153 @@ const Tags = () => {
                         <tbody>
                             <tr>
                                 <td>Install Drupal</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to create a Drupal site.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Drupal update</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to updating any Drupal site to a newer version.
+                                </td>
                             </tr>
                             <tr>
                                 <td>External server</td>
-                                <td></td>
+                                <td> 
+                                    This tag should be used for the tasks, that may be required if the site is not on our own server, or needs to be migrated from another server to our own.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Drupal 7</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required for updating a Drupal 7 site.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Drupal 8</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required for updating a Drupal 8 site.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Migration</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to migrate any content from one site to another.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Blog</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to create a blog.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Webshop</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to create a webshop.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Payment method</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop the payment method of the webshop.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Currency</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to set up the payment details of the webshop.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Custom webhop</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop unique webshop.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Custumer registration</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop user registration.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Product variation</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop variations of the webshop's products.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Invoice system</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop invoice system.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Stock management</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop stock management.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Stock update</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to keep the stock of the webshop fresh.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Additional currencies</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to add a new currency for the webshop.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Additional VAT</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required for adding new tax types.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Coupon system</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop a webshop coupon system.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Product filters</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required for filtering the webshop's products.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Product pages</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop webshop products's details page.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Layout elements</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop layout element system.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Flexible layout</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop flexible layout system.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Unique design</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop the unique design.
+                                </td>
                             </tr>
                             <tr>
                                 <td>Email template</td>
-                                <td></td>
+                                <td>
+                                    This tag should be used for the tasks, that may be required to develop email templates.
+                                </td>
                             </tr>
                         </tbody>
                     </table>
